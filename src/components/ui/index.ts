e6509fe9ad1571d1e116e2b33e7ab0b01aee6e9f
Faysk <faysk.nan@gmail.com>
2026-09-06 23:49:58 +0100

    feat(design): export design-system primitives

---
+++ src/components/ui/index.ts
@@ -0,0 +1,13 @@
+export { ActionLink, Button, actionStyles } from "./action";
+export type { ActionSize, ActionVariant } from "./action";
+export { StatusPill } from "./status";
+export type { StatusTone } from "./status";
+export { Surface } from "./surface";
+export type { SurfaceTone } from "./surface";
+export {
+	BodyCopy,
+	DisplayTitle,
+	Eyebrow,
+	MetaText,
+	SectionTitle,
+} from "./typography";
