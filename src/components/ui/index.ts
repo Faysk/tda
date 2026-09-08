303f7f27518f43676e60420a4a2c9b10d0a304e4
Faysk <faysk.nan@gmail.com>
2026-09-08 16:01:30 +0100

    feat(ui): export themed select primitive

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,5 +1,7 @@
 export { ActionLink, Button, actionStyles } from "./action";
 export type { ActionSize, ActionVariant } from "./action";
+export { Select } from "./select";
+export type { SelectOption } from "./select";
 export { StatusPill } from "./status";
 export type { StatusTone } from "./status";
 export { Surface } from "./surface";
