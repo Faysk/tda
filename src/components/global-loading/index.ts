9866879ec9f62217fc0a8e7f25cbbfc2b0fb5e81
Faysk <faysk.nan@gmail.com>
2026-09-10 05:38:57 +0100

    feat(loading): export global loading API

---
+++ src/components/global-loading/index.ts
@@ -0,0 +1,14 @@
+export {
+	beginGlobalLoadingEvent,
+	beginInteractiveGlobalLoading,
+	GLOBAL_LOADING_START_EVENT,
+	GLOBAL_LOADING_STOP_EVENT,
+	type GlobalLoadingEventDetail,
+} from "./events";
+export {
+	GlobalLoadingProvider,
+	GlobalRouteLoading,
+	useGlobalLoading,
+	useGlobalLoadingFlag,
+	type GlobalLoadingOptions,
+} from "./global-loading";
