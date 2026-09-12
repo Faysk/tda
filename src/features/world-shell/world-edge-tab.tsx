40360037e0267dd08452b07378bfcfd40fd6d198
Faysk <faysk.nan@gmail.com>
2026-09-12 21:10:29 +0100

    style(world): load shared inspector overlay contract

--- src/features/world-shell/world-edge-tab.tsx
+++ src/features/world-shell/world-edge-tab.tsx
@@ -1,4 +1,5 @@
 import type { ReactNode, Ref } from "react";
+import "./world-inspector-overlay-v3.module.css";
 import styles from "./world-edge-tab.module.css";
 
 type WorldEdgeTabProps = Readonly<{
